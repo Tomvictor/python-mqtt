5942ec3ed4a5df8e5a51bda4856c34d61711b4b3
Tomvictor <vjtomvictor@icloud.com>
2016-08-13 22:07:00 +0530

    python scripting ok running  on my mac

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import requests
+import array
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -15,9 +16,14 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print("message recieved")
-    print(msg.topic+" "+str(msg.payload))
+    print("Received message '" + str(msg.payload) + "' on topic '"
+          + msg.topic + "' with QoS " + str(msg.qos))
+    message = str(msg.payload)
+    topic = str(msg.topic)
+    url = "http://iot.buildfromzero.com/store?q=" + message +"&t="+topic
+    print(url)
     #r = requests.get('https://iot.buildfromzero.com/store?q='+ msg.payload + '&t=' + msg.topic)
-    print("https://iot.buildfromzero.com/store?q=",str(msg.payload))
+    requests.get(url)
 
 client = mqtt.Client()
 client.on_connect = on_connect
