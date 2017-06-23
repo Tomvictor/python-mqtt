1e75448228e2bb2728476021a78242575670ffc1
Tom Victor <vjtomvictor@icloud.com>
2017-06-23 11:30:19 +0530

    mqtt code works now

--- only-publish.py
+++ only-publish.py
@@ -1,9 +1,3 @@
-import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-#client = mqtt.Client()
-#client.connect("technoripio.cloudapp.net", 8883, 60)
-#print(client)
-#client.publish("tracker", "temperature")
-
-publish.single("tracker", payload="tom",hostname="technoripio.cloudapp.net",port=8883)
+publish.single("tracker", "payload", hostname="technoripio.cloudapp.net", port=8883)
